a8545a9efa517c228592d5d2fe291113099ac9e0
Christopher Stahle <stahlechris@gmail.com>
2017-09-22 16:44:09 -0500

    Update MusicArtistsApp.java

--- xml_assignment/MusicArtistsApp.java
+++ xml_assignment/MusicArtistsApp.java
@@ -1,16 +1,4 @@
-//***************************************************************************
-//	File:                       MusicArtistsApp.Java
-//
-//	Student:                    Chris Stahle
-//
-//	Assignment:                 Program  # 4
-//
-//	Course Name:                Java Programming I
-//
-//	Course Number:              COSC 2050 - 01
-//
-//      Due:                        November 22, 2016
-//
+
 //      Description:                This program reads an XML file to display
 //                                  a list of artists and listings.
 //***************************************************************************
